08af7e2132e60b4a2d2766a4249220e6aca48609
askonev <askonev007@gmail.com>
2021-03-18 14:53:58 +0300

    Adding read ENV out of file

--- lib/data/static_data.rb
+++ lib/data/static_data.rb
@@ -4,9 +4,20 @@
 class StaticData
   PROJECT_NAME = ''
   DROPLET_NAME_PATTERN = 'droplets-starter'
-
   DROPLET_REGION = 'nyc3'
   DROPLET_IMAGE = 'docker-20-04'
   DROPLET_SIZE = 's-1vcpu-1gb'
   SSH_KEY_ID = ''
+
+  def self.get_project_name
+      return ENV['PROJECT_NAME'] if ENV['PROJECT_NAME']
+
+      File.read("#{Dir.home}/.do/project_name").delete("\n")
+  end
+
+  def self.get_ssh_key_id
+      return ENV['SSH_KEY_ID'] if ENV['SSH_KEY_ID']
+
+      File.read("#{Dir.home}/.do/ssh_key_id").delete("\n")
+  end
 end
--- lib/helpers/rake_helper.rb
+++ lib/helpers/rake_helper.rb
@@ -55,16 +55,16 @@ class RakeHelper
                                         region: StaticData::DROPLET_REGION,
                                         image: StaticData::DROPLET_IMAGE,
                                         size: StaticData::DROPLET_SIZE,
-                                        ssh_keys: [StaticData::SSH_KEY_ID])
+                                        ssh_keys: [StaticData.get_ssh_key_id])
       @init_api.client.droplets.create(droplet)
       @init_api.wait_until_droplet_have_status(loader_name)
     end
 
     def include_in_the_project(droplet_name)
       droplet_id = @init_api.get_droplet_id_by_name(droplet_name)
-      project_id = get_project_id_by_name(StaticData::PROJECT_NAME)
+      project_id = get_project_id_by_name(StaticData.get_project_name)
       @init_api.client.projects.assign_resources(["do:droplet:#{droplet_id}"], id: project_id)
-      logger.info("Droplet #{droplet_name} added by project #{StaticData::PROJECT_NAME}")
+      logger.info("Droplet #{droplet_name} added by project #{StaticData.get_project_name}")
     end
   end
 end
