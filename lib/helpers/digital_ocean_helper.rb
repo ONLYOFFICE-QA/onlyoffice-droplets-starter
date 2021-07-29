fea2e171304c2d403abdc09dc3a2a87620dc053b
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2021-07-29 18:19:39 +0300

    Change the initialized of the DigitalOceanHelper class object (#5)
    * Change the initialized of the DigitalOceanHelper class object
    
    * Rename instance var and init method
    
    * bundle update
    
    * replace method 'get_project_id_by_name'
    
    * Add attr_writer for do_api varible
    
    * Added init DOWrapper into DigitalOceanHelper

--- lib/helpers/digital_ocean_helper.rb
+++ lib/helpers/digital_ocean_helper.rb
@@ -2,69 +2,53 @@
 
 # Class represent rake helper methods
 class DigitalOceanHelper
-  class << self
-    attr_accessor :init_api
+  attr_reader :do_api
 
-    private
+  def initialize(api = OnlyofficeDigitaloceanWrapper::DigitalOceanWrapper.new)
+    @do_api = api
+  end
 
-    def logger
-      @logger ||= Logger.new($stdout)
+  private
+
+  def logger
+    @logger ||= Logger.new($stdout)
+  end
+
+  # @return [Array<String>] names of currently run loaders
+  def loaders_names
+    loaders = []
+    all_droplets = @do_api.client.droplets.all
+    all_droplets.each do |droplet|
+      loaders << droplet.name if droplet.name.start_with?(StaticData::DROPLET_NAME_PATTERN)
     end
+    loaders
+  end
 
-    def project_by_name(project_name)
-      @init_api.retry_exception do
-        projects = @init_api.client.projects.all
-        projects.find { |x| x.name == project_name }
-      end
-    end
+  public
 
-    def get_project_id_by_name(project_name)
-      project = project_by_name(project_name)
-      if project.nil?
-        logger.info("get_project_id_by_name(#{project_name}): not found any projects")
-        nil
-      else
-        logger.info("get_project_id_by_name(#{project_name}): #{project.id}")
-        project.id
-      end
-    end
+  # @return [String] next name of loader
+  def next_loader_name
+    loaders = loaders_names
+    return "#{StaticData::DROPLET_NAME_PATTERN}-0" if loaders.empty?
 
-    # @return [Array<String>] names of currently run loaders
-    def loaders_names
-      loaders = []
-      all_droplets = @init_api.client.droplets.all
-      all_droplets.each do |droplet|
-        loaders << droplet.name if droplet.name.start_with?(StaticData::DROPLET_NAME_PATTERN)
-      end
-      loaders
-    end
+    loaders_digits = loaders.map { |x| x[/\d+/].to_i }
+    "#{StaticData::DROPLET_NAME_PATTERN}-#{loaders_digits.max + 1}"
+  end
 
-    public
+  def create_droplet(loader_name)
+    droplet = DropletKit::Droplet.new(name: loader_name,
+                                      region: StaticData::DROPLET_REGION,
+                                      image: StaticData::DROPLET_IMAGE,
+                                      size: StaticData::DROPLET_SIZE,
+                                      ssh_keys: [StaticData.get_ssh_key_id])
+    @do_api.client.droplets.create(droplet)
+    @do_api.wait_until_droplet_have_status(loader_name)
+  end
 
-    # @return [String] next name of loader
-    def next_loader_name
-      loaders = loaders_names
-      return "#{StaticData::DROPLET_NAME_PATTERN}-0" if loaders.empty?
-
-      loaders_digits = loaders.map { |x| x[/\d+/].to_i }
-      "#{StaticData::DROPLET_NAME_PATTERN}-#{loaders_digits.max + 1}"
-    end
-
-    def create_droplet(loader_name)
-      droplet = DropletKit::Droplet.new(name: loader_name,
-                                        region: StaticData::DROPLET_REGION,
-                                        image: StaticData::DROPLET_IMAGE,
-                                        size: StaticData::DROPLET_SIZE,
-                                        ssh_keys: [StaticData.get_ssh_key_id])
-      @init_api.client.droplets.create(droplet)
-      @init_api.wait_until_droplet_have_status(loader_name)
-    end
-
-    def include_in_the_project(droplet_name)
-      droplet_id = @init_api.get_droplet_id_by_name(droplet_name)
-      project_id = get_project_id_by_name(StaticData.get_project_name)
-      @init_api.client.projects.assign_resources(["do:droplet:#{droplet_id}"], id: project_id)
-      logger.info("Droplet #{droplet_name} added by project #{StaticData.get_project_name}")
-    end
+  def include_in_the_project(droplet_name)
+    droplet_id = @do_api.get_droplet_id_by_name(droplet_name)
+    project_id = @do_api.get_project_id_by_name(StaticData.get_project_name)
+    @do_api.client.projects.assign_resources(["do:droplet:#{droplet_id}"], id: project_id)
+    logger.info("Droplet #{droplet_name} added by project #{StaticData.get_project_name}")
   end
 end
