7ca56bffad01969e5cfd296de2a951b03986489b
Sorokin_Maksim <askonev007@gmail.com>
2021-11-24 12:13:01 +0300

    revert the droplet name

--- lib/data/static_data.rb
+++ lib/data/static_data.rb
@@ -3,7 +3,7 @@
 # class with some constants and static data
 class StaticData
   PROJECT_NAME = ''
-  DROPLET_NAME_PATTERN = 'convert-service'
+  DROPLET_NAME_PATTERN = 'droplets-starter'
   DROPLET_REGION = 'nyc3'
   DROPLET_IMAGE = 'docker-20-04'
   DROPLET_SIZE = 's-1vcpu-1gb'
