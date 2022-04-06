ebce14d0351431bf336d124ae7139c06dacd882a
maks_sorokin <askonev007@gmail.com>
2022-04-06 17:22:02 +0300

    wrap file manager methods in class << self

--- lib/helpers/file_manager.rb
+++ lib/helpers/file_manager.rb
@@ -4,25 +4,27 @@ require 'logger'
 
 # Describe
 class FileManager
-  # @param [Object] data
-  # @param [Object] pattern
-  # @param [Object] changes
-  # @return [Object]
-  def self.overwrite(data, pattern, changes)
-    case changes
-    when String
-      data = data.sub(pattern, "\"#{changes}\"")
-      Logger.new(@stdout).info 'is overwritten by the pattern'
-    when Array
-      if data.scan(pattern).length == changes.length
-        changes.each do |path|
-          data = data.sub(pattern, "\"#{File.read("#{ENV['HOME']}/#{path[:dir]}/#{path[:file]}").rstrip}\"")
-          Logger.new(@stdout).info "#{path[:file]} is written"
+  class << self
+    # @param [Object] data
+    # @param [Object] pattern
+    # @param [Object] changes
+    # @return [Object]
+    def overwrite(data, pattern, changes)
+      case changes
+      when String
+        data = data.sub(pattern, "\"#{changes}\"")
+        Logger.new(@stdout).info 'is overwritten by the pattern'
+      when Array
+        if data.scan(pattern).length == changes.length
+          changes.each do |path|
+            data = data.sub(pattern, "\"#{File.read("#{ENV['HOME']}/#{path[:dir]}/#{path[:file]}").rstrip}\"")
+            Logger.new(@stdout).info "#{path[:file]} is written"
+          end
         end
+      else
+        Logger.new(@stdout).error 'Overwrite error'
       end
-    else
-      Logger.new(@stdout).error 'Overwrite error'
+      data
     end
-    data
   end
 end
