ffede2dd275262938a97bfae43e64db373ea83e2
maks_sorokin <askonev007@gmail.com>
2022-04-20 20:25:29 +0300

    Added wrapper over linked classes

--- lib/helpers/remote_configuration.rb
+++ lib/helpers/remote_configuration.rb
@@ -5,30 +5,41 @@ require_relative '../management'
 # Describer
 class RemoteConfiguration
   attr_reader :version, :spec, :host
-
   def initialize(args)
     @host       = args[:host]
     @version    = args[:version]
     @spec_name  = args[:spec]
   end
 
-  # @return [Array, Net::SSH::Authentication]
-  def convert_service_testing
-    SshClient.new.connect(host, StaticData::DEFAULT_USER, {}) do |session|
-      SshClient.run(session, StaticData::SWAP)
-      logger.info session.exec! StaticData::GIT_CLONE_PROJECT
-      dockerfile = SftpClient.download!(session, StaticData::DOCKERFILE)
-      SftpClient.upload!(session,
-                         StaticData::DOCKERFILE,
-                         FileManager.overwrite(dockerfile, /""/, StaticData::PATHS_LIST))
-      env = SftpClient.download!(session, StaticData::ENV)
-      SftpClient.upload!(session,
-                         StaticData::ENV,
-                         env = FileManager.overwrite(env, /latest/, @version))
-      SftpClient.upload!(session,
-                         StaticData::ENV,
-                         FileManager.overwrite(env, /''/, @spec_name))
-      logger.info session.exec! 'cd convert-service-testing/; docker-compose up -d'
+  def ssh(&block)
+    @ssh ||= SshWrapper.new(host, StaticData::DEFAULT_USER, {}, &block)
+  end
+
+  def f_manager
+    @f_manager ||= FileManager.new
+  end
+
+  def build_convert_service_testing
+    ssh do |ch|
+      ssh.execute_in_shell!(ch, File.read(StaticData::SWAP))
+      ch.exec!(StaticData::GIT_CLONE_PROJECT)
+
+      dockerfile = ssh.download!(ch, StaticData::DOCKERFILE)
+      ssh.upload!(ch,
+                  StaticData::DOCKERFILE,
+                  f_manager.overwrite(dockerfile, /""/, StaticData::PATHS_LIST))
+
+      env = ssh.download!(ch, StaticData::ENV)
+      ssh.upload!(ch,
+                  StaticData::ENV,
+                  env = f_manager.overwrite(env, /latest/, @version))
+      ssh.upload!(ch,
+                  StaticData::ENV,
+                  f_manager.overwrite(env, /''/, @spec_name))
+
+      ch.exec!('cd convert-service-testing/; docker-compose up -d') do |ch2, stream, data|
+        $stdout << data if stream == :stdout
+      end
     end
   end
 end
