877a9b3e7bea6151a02325cef1d2453c2ac29275
l02 <l8556@outlook.com>
2023-07-26 19:45:12 +0300

    Fix update docker

--- lib/bash_scripts/script.sh
+++ lib/bash_scripts/script.sh
@@ -12,7 +12,7 @@ service ufw stop
 ufw disable
 apt-get update
 apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-apt-get install docker-ce -y
+yes Y | apt-get install docker-ce -y
 apt-get install docker-compose -y
 docker run -itd -p 80:80 --name 'droplet_starter' onlyoffice/4testing-documentserver-ee:$DS_VERSION
 sleep 90
